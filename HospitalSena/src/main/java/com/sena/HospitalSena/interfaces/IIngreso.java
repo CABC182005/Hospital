b4efc45593dd7d486ff885d5a5ae47cb0ccb9e31
SENA <SENA@NEIDFDSITFSD713.FormacionSena.RED>
2024-03-18 11:37:29 -0500

    solo un paciente por cama

--- HospitalSena/src/main/java/com/sena/HospitalSena/interfaces/IIngreso.java
+++ HospitalSena/src/main/java/com/sena/HospitalSena/interfaces/IIngreso.java
@@ -1,11 +1,13 @@
 package com.sena.HospitalSena.interfaces;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import com.sena.HospitalSena.models.ingreso;
 
 
 @Repository
 public interface IIngreso extends CrudRepository<ingreso,String>{
-
+	@Query(value = "SELECT COUNT(*) FROM ingresos WHERE id_paciente = :idPaciente AND cama = :cama AND estado = true", nativeQuery = true)
+    Double contador(String cama, Long idPaciente);
 }
